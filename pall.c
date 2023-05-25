675a309b2ca890cdd641df999c16fde163c4c1bc
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-25 01:44:32 -0700

    updated pall.c

--- pall.c
+++ pall.c
@@ -1,1 +1,20 @@
-
+#include "monty.h"
+/**
+* pall - prints all elements in  the stack
+* @head: pointer to the top node
+* @line_number: number used in bytecode file
+* Return: nothing
+*/
+void pall(stack_t **head, unsigned int line_number)
+{
+stack_t *h;
+(void)line_number;
+h = *head;
+if (h == NULL)
+return;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+}
