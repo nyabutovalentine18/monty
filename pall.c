3e8f81e539d87bd187a127c99cb6ea9d520a10fa
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-25 03:40:14 -0700

    updated pall.c

--- pall.c
+++ pall.c
@@ -8,13 +8,15 @@
 void pall(stack_t **head, unsigned int line_number)
 {
 stack_t *h;
-(void)line_number;
-h = *head;
-if (h == NULL)
-return;
+h  = *stack;
+(void) line_number;
+if (*stack)
+{
+h  = *stack;
 while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
+}
 }
 }
